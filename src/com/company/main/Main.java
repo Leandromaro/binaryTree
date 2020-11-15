7ac447a4974f68c0dc9e3cab9d4ee170b42bf76e
Eugenia Nunez <eugeiq@gmail.com>
2020-11-15 12:30:05 -0300

    Adding examples and images

--- src/com/company/main/Main.java
+++ src/com/company/main/Main.java
@@ -5,13 +5,11 @@ public class Main {
     public static void main(String[] args) {
         BinaryTree bt = new BinaryTree();
 
-        bt.add(6);
-        bt.add(4);
-        bt.add(8);
         bt.add(3);
         bt.add(5);
         bt.add(7);
-        bt.add(9);
+
+        bt.add(6);
 
         System.out.println(bt.containsNode(9));
 
